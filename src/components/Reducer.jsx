0505d1de2b67f28c3b8e62495a4d3e16da4d9658
gitsibi <159125779+gitsibi@users.noreply.github.com>
2024-07-13 11:49:16 +0530

    Update Reducer.jsx

--- src/components/Reducer.jsx
+++ src/components/Reducer.jsx
@@ -1,20 +1,22 @@
-/* eslint-disable react-refresh/only-export-components */
 const startingValue = {
-    count:0
-}
-const Reducer = (state=startingValue,action) => {
-    switch (action.type) {
-        case "INCREMENT":
-          return {
-            ...state,
-            count: state.count + 1,};
-        case "DECREMENT":
-          return {
-            ...state,
-            count: state.count > 0 ? state.count - 1 : 0,};
-        default:
-          return state;
-      }
-}
+  count: 0
+};
 
-export { startingValue, Reducer };
+const reducer = (state = startingValue, action) => {
+  switch (action.type) {
+      case "INCREMENT":
+          return {
+              ...state,
+              count: state.count + 1
+          };
+      case "DECREMENT":
+          return {
+              ...state,
+              count: state.count > 0 ? state.count - 1 : 0
+          };
+      default:
+          return state;
+  }
+};
+
+export { startingValue, reducer };
